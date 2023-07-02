bec57f9ad3dc87395e4de6772e2ca5326563b44c
Seongjung Kim <vder8282@gmail.com>
2023-07-02 18:19:13 +0900

    feat Live2D character expression #25

--- Assets/_MAIN/Scripts/Core/Characters/Character Types/Character_Live2D.cs
+++ Assets/_MAIN/Scripts/Core/Characters/Character Types/Character_Live2D.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Live2D.Cubism.Rendering;
+using Live2D.Cubism.Framework.Expression;
 
 namespace CHARACTERS {
     public class Character_Live2D : Character {
+        private CubismRenderController renderController;
+        private CubismExpressionController expressionController;
         private Animator motionAnimator;
 
         public Character_Live2D(string name, CharacterConfigData config, GameObject prefab, string rootAssetsFolder) : base(name, config, prefab) {
             Debug.Log($"Created Live2D Character: '{name}'");
             motionAnimator = animator.transform.GetChild(0).GetComponentInChildren<Animator>();
+            renderController = motionAnimator.GetComponent<CubismRenderController>();
+            expressionController = motionAnimator.GetComponent<CubismExpressionController>();
         }
 
         public void SetMotion(string animationName) {
             motionAnimator.Play(animationName);
+        }
+
+        public void SetExpression(int expressionIndex) {
+            expressionController.CurrentExpressionIndex = expressionIndex;
+        }
+
+        public void SetExpression(string expressionName) {
+            expressionController.CurrentExpressionIndex = GetExpressionIndexByName(expressionName);
+        }
+
+        private int GetExpressionIndexByName(string expressionName) {
+            expressionName = expressionName.ToLower();
+
+            for (int i = 0; i < expressionController.ExpressionsList.CubismExpressionObjects.Length; i++) {
+                CubismExpressionData expr = expressionController.ExpressionsList.CubismExpressionObjects[i];
+                if (expr.name.Split('.')[0].ToLower() == expressionName)
+                    return i;
+            }
+
+            return -1;
         }
     }
 }
